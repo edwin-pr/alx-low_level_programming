edd395caa8ff724778c3fc83f0af573ecedd48a6
edwin-pr <edwingitau100@gmail.com>
2023-03-16 08:07:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
-* main - A program that prints a line using the printf function
-* Return : 0, (Success)
+*main-block code
+*Return: 0, on success
 */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
