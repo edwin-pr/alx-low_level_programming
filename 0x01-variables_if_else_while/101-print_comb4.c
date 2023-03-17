f2917d81901fa24a681624fec7aa75a57c429a98
edwin-pr <edwingitau100@gmail.com>
2023-03-17 16:09:22 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - print all combination of three different digits
+ * Return: 0 always on success
+ */
+
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0'; hundred <= '9'; hundred++)/*hundred place*/
+	{
+		for (ten = (hundred + 1); ten <= '9'; ten++)/*tens=100s+1*/
+		{
+			for (one = (ten + 1); one <= '9'; one++)/*ones*/
+			{
+				putchar(hundred);
+				putchar(ten);
+				putchar(one);
+				if (hundred != '7' || ten != '8' || one != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
