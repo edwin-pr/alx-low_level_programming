c0b0109015062bff53c10177e1bd4fc9d20e562c
edwin-pr <edwingitau100@gmail.com>
2023-03-17 13:11:04 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ *Return: 0 always successful
+ */
+int main(void)
+{
+	char i
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+
+}
