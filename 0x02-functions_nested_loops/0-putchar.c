5d66f57d4b3108181ec0c6a109af99d5b9c0f623
edwin-pr <edwingitau100@gmail.com>
2023-03-21 05:43:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by edwin
+ * Return:0  on  success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
