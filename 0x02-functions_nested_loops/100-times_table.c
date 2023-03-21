83982efd44f8001bda506cf7fce5e85f0bb19776
edwin-pr <edwingitau100@gmail.com>
2023-03-21 11:12:33 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 	for (num = 0; num <= n; num++)
 	{
 	_putchar('0');
-	for (mult = o; mult <= n; mult++)
+	for (mult = 0; mult <= n; mult++)
 	{
 	_putchar(',');
 	_putchar(' ');
