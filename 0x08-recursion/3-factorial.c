7aa8c8c5032078a82a50fa01fa6cf782a64c3d19
edwin-pr <edwingitau100@gmail.com>
2023-04-05 02:29:13 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - entry point
+ * @n: integer factorial value
+ * Return: always on success
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
