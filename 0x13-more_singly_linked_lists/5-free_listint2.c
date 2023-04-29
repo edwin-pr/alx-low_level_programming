e9bdcae90a2d27f5763914885f4414d93ae61057
edwin-pr <edwingitau100@gmail.com>
2023-04-29 10:15:16 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t list
+ *
+ * @head: head.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *c, *tmp;
+
+	/* Free each node in the list */
+	c = *head;
+	while (c != NULL)
+	{
+		tmp = c;
+		c = c->next;
+		free(tmp);
+	}
+
+	/* Set head to NULL */
+	*head = NULL;
+}
