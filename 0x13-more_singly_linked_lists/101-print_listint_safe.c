e744d5ea7ab2fe2c434949ed6c24b0419648465f
edwin-pr <edwingitau100@gmail.com>
2023-04-30 07:52:40 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,33 @@
 #include "lists.h"
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: A pointer to the head of the list
+ * print_listint_safe - Prints a linked list of integers safely
+ * @head: A pointer to the first node of the linked list
  *
- * Return: The number of nodes in the list
+ * Return: The number of nodes in the linked list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *s = head, *f = head;
-	size_t c = 0;
+	size_t num_nodes = 0;
+	const listint_t *current_node = head, *next_node;
 
-	if (head == NULL)
-		return (0);
-
-	while (s != NULL && f != NULL && f->next != NULL)
+	while (current_node != NULL)
 	{
-		s = s->next;
-		f = f->next->next;
+		num_nodes++;
+		printf("[%p] %d\n", (void *)current_node, current_node->n);
 
-		if (s == f)
+		next_node = current_node->next;
+
+		if (next_node >= current_node)
 		{
-			printf("-> [%p] %d\n", (void *)s, s->n);
-			c++;
-			while (s != head)
-			{
-				c++;
-				printf("[%p] %d\n", (void *)head, head->n);
-				head = head->next;
-			}
-			printf("[%p] %d\n", (void *)head, head->n);
-			return (c);
+		current_node = next_node;
 		}
-		printf("[%p] %d\n", (void *)s, s->n);
-		c++;
+		else
+		{
+		printf("-> [%p] %d\n", (void *)next_node, next_node->n);
+		break;
+		}
 	}
 
-	while (head != NULL)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		c++;
-		head = head->next;
-	}
-
-	return (c);
+	return (num_nodes);
 }
