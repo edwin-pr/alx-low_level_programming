b91ab7432691c31c27f81682bdb093cc7cb00dd7
edwin-pr <edwingitau100@gmail.com>
2023-05-08 13:33:26 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, num_bytes_read, num_bytes_written;
+	char buffer[BUFFER_SIZE];
+	const char *file_from, *file_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+
+	file_from = argv[1];
+	file_to = argv[2];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+    {
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((num_bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		num_bytes_written = write(fd_to, buffer, num_bytes_read);
+		if (num_bytes_written != num_bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (num_bytes_read == -1)
+	{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			close(fd_from);
+			close(fd_to);
+			exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return 0;
+}
